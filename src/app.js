9a7815d38f346dba670095e652c88218a9f64bf4
sadiq44 <sadiq447@gmail.com>
2025-12-29 11:22:49 +0000

    fix app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import logger from '#config/logger.js';
-import helmet from "helmet";
+import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
@@ -8,10 +8,10 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(helmet());
-app.use(cors);
-app.use(express.json);
+//app.use(cors);
+//app.use(express.json);
 app.use(express.urlencoded( {extended: true}));
-app.use(cookieParser);
+//app.use(cookieParser);
 
 app.use(morgan('combined', { stream: {write: (message) => logger.info(message.trim)}}));
 
