b9533412e587ffd5e4b2fae23d2448b9f87b8441
sadiq44 <sadiq447@gmail.com>
2026-01-02 20:10:39 +0430

    update

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded( {extended: true}));
 app.use(cookieParser());
 
-app.use(morgan('combined', { stream: {write: (message) => logger.info(message.trim)}}));
+app.use(morgan('combined', { stream: {write: (message) => logger.info(message.trim())}}));
 
 app.get('/', (req, res) => {
   logger.info('Hello from logger');
